2893ed07e59d2e524bba16f2b067c2eb45e346a1
menib <meni.b@qualisystems.com>
2018-10-24 10:16:31 +0300

    Update rails_website.sh

--- applications/rails-website/rails_website.sh
+++ applications/rails-website/rails_website.sh
@@ -53,7 +53,6 @@ echo "****************************************************************"
 echo "Installing website (from artifacts) and retrieving dependencies"
 echo "****************************************************************"
 mkdir /home/artifacts
-mkdir /home/artifacts
 cd /home/artifacts || exit
 git clone https://github.com/cloudshell-colony/sample_rails_source.git
 
